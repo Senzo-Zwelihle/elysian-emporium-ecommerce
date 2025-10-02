1d68956113f27874a699a1930847d7e29d791aae
Senzo-Zwelihle <senzozwelihlemasango@gmail.com>
2025-10-02 16:59:52 +0200

    oauth api tested and working

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Store = () => {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+      <h1 className="text-9xl">Home</h1>
+    </div>
+  )
+}
+
+export default Store
